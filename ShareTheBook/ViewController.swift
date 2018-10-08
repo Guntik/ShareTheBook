c74a47a0e3afa624d669a9bbb7f4a73244115259
Damir Gainetdinov <damir@adjust.com>
2018-10-08 11:18:49 +0200

    Initial Commit

---
+++ ShareTheBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareTheBook
+//
+//  Created by Damir Gaynetdinov on 08/10/2018.
+//  Copyright © 2018 Galina Gainetdinova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
